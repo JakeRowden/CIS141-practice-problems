7ba525988de85a7d8313ca7522861ab88eb91eeb
Jacob Rowden <rowdenjacob@gmail.com>
2025-04-20 13:51:19 -0700

    problem1.py

---
+++ Mod2/problem1.py
@@ -0,0 +1,3 @@
+#A program that prints the introduction of Avatar The Last Airbender to the terminal
+
+print("Water. Earth. Fire. Air. Long ago, the four nations lived together in harmony. Then, everything changed when the Fire Nation attacked.")
