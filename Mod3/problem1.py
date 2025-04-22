3cb2b377fe226bcbb365441741e4913d36887958
Jacob Rowden <rowdenjacob@gmail.com>
2025-04-21 19:47:28 -0700

    Create problem1.py

---
+++ Mod3/problem1.py
@@ -0,0 +1,8 @@
+#prompts for a word then asks how many times you would like it repeated
+
+#Word and how many times to repeat
+word = input("Please provide a word")
+multiplier = int(input("How many times would you like me to repeat " + word))
+
+#Prints the word the desired number of times
+print(word * multiplier)
