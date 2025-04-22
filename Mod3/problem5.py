8c79fd86b6e14c03c2f510a5c1656ada201b0552
Jacob Rowden <rowdenjacob@gmail.com>
2025-04-21 19:53:20 -0700

    Create problem5.py

---
+++ Mod3/problem5.py
@@ -0,0 +1,9 @@
+#Prints 3 words with a | character 
+
+#3 words split and then joined with a |
+phrase = "bacon is delicious"
+phrase_2 = phrase.split()
+phrase_3 = "|".join(phrase_2)
+
+#prints the final result
+print(phrase_3)
