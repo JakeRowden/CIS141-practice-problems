03c2d7bab93125f0a42ae0ad489c94493f97fbec
Jacob Rowden <rowdenjacob@gmail.com>
2025-04-29 10:40:59 -0700

    problem1.py

---
+++ Mod4/problem1.py
@@ -0,0 +1,26 @@
+#Truth table for the expression: (A AND B) OR (NOT B) where A and B can be True or False
+
+# 1.List Variables
+
+#A,B
+
+#2. Number of Rows
+
+# 2 possibilites for A
+# 2 possibilities for B
+# 2x2 = 4
+
+#3. Create table columns
+
+#A,B     (A AND B)        (NOT B)       (A AND B) OR (NOT B)
+
+#4. Generate all possible combinations of (A, B)
+
+
+#5. Fill each Row with Sub-Expression Results
+
+#A B     (A AND B)   (NOT B)      (A AND B) OR (NOT B)
+#T T      #T          #F           #T
+#T F      #F          #T           #T
+#F T      #F          #F           #F
+#F F      #F          #T           #T
