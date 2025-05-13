7550e9a921a51b12dad9ee2521ba4f6bab30320f
Jacob Rowden <rowdenjacob@gmail.com>
2025-05-13 15:59:13 -0700

    Update problem3.py

--- Mod5/problem3.py
+++ Mod5/problem3.py
@@ -1,7 +1,4 @@
 #Prints all even numbers from 2 to 20 using a for loop and range function
 
-for i in range(20):
-    i +=1
-    if i %2 != 0 :
-        continue
-    print(i)
+for i in range(2,21,2):
+ print(i)
