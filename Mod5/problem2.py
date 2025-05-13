f5032c8331c58b5f417851d28ada688f1329bdf1
Jacob Rowden <rowdenjacob@gmail.com>
2025-05-12 18:46:28 -0700

    Create problem2.py

---
+++ Mod5/problem2.py
@@ -0,0 +1,13 @@
+#Prints each string character of "The Dog" on its own line
+
+phrase = str("The Dog")
+character_amount = len(phrase)
+
+
+i = 0
+
+while i < character_amount:
+    print(phrase[i])
+    i+=1
+    
+    
