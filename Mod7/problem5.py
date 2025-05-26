dc352d9d8367ee5a68829b0883988c13b6c29c12
Jacob Rowden <rowdenjacob@gmail.com>
2025-05-25 22:29:54 -0700

    Create problem5.py

---
+++ Mod7/problem5.py
@@ -0,0 +1,20 @@
+'''
+#5. A function called level_up(experience) that takes a player's experience
+points and returns their new level based on these rules:
+* 0-99 XP → Level 1
+* 100-199 XP → Level 2
+* 200+ XP → Level 3
+'''
+
+def level_up(experience):
+    if experience < 100:
+        return "Level 1"
+    if experience >99 and experience < 200:
+        return "Level 2"
+    if experience >= 200:
+        return "Level 3"
+    
+print(level_up(200))
+print(level_up(150))
+print(level_up(3))
+print(level_up(79))
